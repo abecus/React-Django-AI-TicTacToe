ab5da01af169c1f6011542ca0c627b1d81adeb32
abecus <insaaone@gmail.com>
2020-11-25 16:50:52 +0530

    .

--- tictactoe/settings.py
+++ tictactoe/settings.py
@@ -61,8 +61,8 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
-            os.path.join(BASE_DIR, "build", "static"),
-            os.path.join(BASE_DIR, "build"),
+            # os.path.join(BASE_DIR, "build", "static"),
+            # os.path.join(BASE_DIR, "build"),
         ],
         "APP_DIRS": True,
         "OPTIONS": {
